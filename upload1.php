a7c579076d3a3db4c28491d8dcc29b37194bf328
Masnapos <130586665+Masnapos@users.noreply.github.com>
2023-04-15 18:23:51 +0200

    upload

--- upload1.php
+++ upload1.php
@@ -1,32 +1,92 @@
 <?php
+// Database credentials
 $servername = "localhost";
-$username = "your_username";
-$password = "your_password";
-$dbname = "image_upload";
+$username = "root";
+$password = "";
+$dbname = "image_database";
 
+// Create a connection to the database
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Check the connection
 if ($conn->connect_error) {
-  die("Connection failed: " . $conn->connect_error);
+    die("Connection failed: " . $conn->connect_error);
 }
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $image = $_FILES["image"];
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $target_dir = "uploads/";
+    $target_file = $target_dir . basename($_FILES["image"]["name"]);
+    $uploadOk = 1;
+    $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
 
-  $target_dir = "uploads/";
-  $target_file = $target_dir . basename($image["name"]);
+    // Check if image file is an actual image or a fake image
+    $check = getimagesize($_FILES["image"]["tmp_name"]);
+    if ($check !== false) {
+        echo "File is an image - " . $check["mime"] . ".";
+        $uploadOk = 1;
+    } else {
+        echo "File is not an image.";
+        $uploadOk = 0;
+    }
 
-  if (move_uploaded_file($image["tmp_name"], $target_file)) {
-    $sql = "INSERT INTO images (image_name, image_path) VALUES (?, ?)";
-    $stmt = $conn->prepare($sql);
-    $stmt->bind_param("ss", $image["name"], $target_file);
-    $stmt->execute();
-    $stmt->close();
-    header("Location: upload.html");
-  } else {
-    echo "Sorry, there was an error uploading your file.";
-  }
+    // Check if file already exists
+    if (file_exists($target_file)) {
+        echo "Sorry, file already exists.";
+        $uploadOk = 0;
+    }
+
+    // Check file size
+    if ($_FILES["image"]["size"] > 500000) {
+        echo "Sorry, your file is too large.";
+        $uploadOk = 0;
+    }
+
+    // Allow certain file formats
+    if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg" && $imageFileType != "gif") {
+        echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+        $uploadOk = 0;
+    }
+
+    // Check if $uploadOk is set to 0 by an error
+    if ($uploadOk == 0) {
+        echo "Sorry, your file was not uploaded.";
+    } else {
+        if (move_uploaded_file($_FILES["image"]["tmp_name"], $target_file)) {
+            echo "The file " . basename($_FILES["image"]["name"]) . " has been uploaded.";
+
+            // Set the image information
+            $title = "Image Title"; // Replace this with the desired image title
+            $description = "Image Description"; // Replace this with the desired image description
+            $image_path = $target_file;
+
+            // Prepare and bind the SQL statement
+            $stmt = $conn->prepare("INSERT INTO images (title, description, image_path) VALUES (?, ?, ?)");
+            $stmt->bind_param("sss", $title, $description, $image_path);
+
+            // Execute the statement
+            $stmt->execute();
+
+            // Close the statement
+            $stmt->close();
+
+        } else {
+            echo "Sorry, there was an error uploading your file.";
+        }
+    }
 }
 
+// Fetch and display images
+$sql = "SELECT * FROM images";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo '<img src="' . $row["image_path"] . '">';
+    }
+} else {
+    echo "No images found.";
+}
+
+// Close the database connection
 $conn->close();
 ?>

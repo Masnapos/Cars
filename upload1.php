45f24ea9f6f29a30a7d1edde52d5c75dad5db910
Masnapos <130586665+Masnapos@users.noreply.github.com>
2023-04-15 18:45:51 +0200

    0

--- upload1.php
+++ upload1.php
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     }
 
     // Check file size
-    if ($_FILES["image"]["size"] > 500000) {
+    if ($_FILES["image"]["size"] > 500000000) {
         echo "Sorry, your file is too large.";
         $uploadOk = 0;
     }

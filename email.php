96597c04f87eee1e92b1c6900112ff55e954019e
KlucsikAdam <128250004+KlucsikAdam@users.noreply.github.com>
2023-05-04 21:58:24 +0200

    fsf

---
+++ email.php
@@ -0,0 +1,22 @@
+<!doctype html>
+<html>
+
+<body>
+<?php
+if (isset($_POST['submit'])){
+echo $_POST['name'];
+echo "<br>";
+echo $_POST['email'];
+echo "<br>";
+echo $_POST['subject'];
+echo "<br>";
+echo $_POST['text'];
+
+
+}
+
+
+
+?>
+</body>
+</html>
